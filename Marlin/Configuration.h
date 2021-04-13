eb75cbff59dcc391d3e2861a68c2ff710b6f838c
Taomyn <ferd352+github@gmail.com>
2021-04-13 15:05:09 +0200

    Update Configuration.h
    Enabled acceleration changes through LCD

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -821,7 +821,7 @@
  */
 #define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
 
-//#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
+#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
   #define MAX_ACCEL_EDIT_VALUES       { 6000, 6000, 200, 20000 } // ...or, set your own edit limits
 #endif
